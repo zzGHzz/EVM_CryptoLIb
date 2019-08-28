89cd86cd3ca439453ac6850ddfc68ecc3a83991f
starshine87 <starshine87@sjtu.edu.cn>
2019-08-29 04:53:57 +0800

    add curve c++ part

--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,40 @@
+#include "include/secp256k1.h"
 #include <iostream>
+#include <cassert>
+#include <string>
 
 using namespace std;
 
+string bin[16] = {"0000", "0001", "0010", "0011",
+                  "0100", "0101", "0110", "0111",
+                  "1000", "1001", "1010", "1011",
+                  "1100", "1101", "1110", "1111"};
+
+bool check(Secp256k1_point now, string check) {
+    return true;
+}
+
 int main()
 {
-    
+    Secp256k1_point g;
+    string s = "1";
+    const vector<unsigned char> n(s.begin(), s.end());
+    g.set_ng(n);
+    Secp256k1_point o;
+    o.set_O();
+    for (int i=2; i<o.ORDER.length(); ++i) {
+        string tmp = "";
+        tmp += o.ORDER[i];
+        int pos = stoi(tmp, nullptr, 16);
+        for (int j=0; j<4; ++j) {
+            o+=o;
+            check(o, ""); // TODO
+            if (bin[pos][j] == '1') {
+                o+=g;
+                check(o, ""); //TODO
+            }
+        }
+    }
+    assert(o.is_O());
+    return 0;
 }
--- test/secp256k1.cpp
+++ test/secp256k1.cpp
@@ -4,13 +4,13 @@
 
 #include "include/secp256k1.h"
 
-#include <string>
 #include <cassert>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-void secp256k1_point::init()
+void Secp256k1_point::init()
 {
     group = NULL;
     point = NULL;
@@ -37,28 +37,38 @@ void secp256k1_point::init()
     return;
 }
 
-secp256k1_point::secp256k1_point(const secp256k1_point& source)
+Secp256k1_point::Secp256k1_point(const Secp256k1_point& src)
 {
     init();
-    if (!EC_GROUP_copy(group, source.group)) throw std::runtime_error("secp256k1_point::secp256k1_point(const secp256k1_point&) - EC_GROUP_copy failed.");
-    if (!EC_POINT_copy(point, source.point)) throw std::runtime_error("secp256k1_point::secp256k1_point(const secp256k1_point&) - EC_POINT_copy failed.");
+    if (!EC_GROUP_copy(group, src.group))
+        throw runtime_error("secp256k1_point::secp256k1_point(copy): EC_GROUP_copy failed!!");
+    if (!EC_POINT_copy(point, src.point))
+        throw runtime_error("secp256k1_point::secp256k1_point(copy): EC_POINT_copy failed!!");
 }
 
-secp256k1_point::secp256k1_point(const bytes_t& bytes)
-{
+Secp256k1_point::Secp256k1_point(const vector<unsigned char> &bytes) {
     init();
-    this->bytes(bytes);
+    BIGNUM* bn = BN_bin2bn(&bytes[0], bytes.size(), NULL);
+    if (!bn) {
+        throw runtime_error("secp256k1_point::secp256k1_point(bin): BN_bin2bn failed!!");
+    }
+    if (!EC_POINT_bn2point(group, bn, point, ctx)) {
+        BN_clear_free(bn);
+        throw runtime_error("secp256k1_point::secp256k1_point(bin): EC_POINT_bn2point failed!!");
+    }
 }
 
-
-secp256k1_point::~secp256k1_point()
+Secp256k1_point::~Secp256k1_point()
 {
-    if (point) EC_POINT_free(point);
-    if (group) EC_GROUP_free(group);
-    if (ctx)   BN_CTX_free(ctx);
+    if (point)
+        EC_POINT_free(point);
+    if (group)
+        EC_GROUP_free(group);
+    if (ctx)
+        BN_CTX_free(ctx);
 }
 
-secp256k1_point& secp256k1_point::operator=(const secp256k1_point& rhs)
+Secp256k1_point& Secp256k1_point::operator=(const Secp256k1_point& rhs)
 {
     if (!EC_GROUP_copy(group, rhs.group)) throw std::runtime_error("secp256k1_point::operator= - EC_GROUP_copy failed.");
     if (!EC_POINT_copy(point, rhs.point)) throw std::runtime_error("secp256k1_point::operator= - EC_POINT_copy failed.");
@@ -66,66 +76,25 @@ secp256k1_point& secp256k1_point::operator=(const secp256k1_point& rhs)
     return *this;
 }
 
-void secp256k1_point::bytes(const bytes_t& bytes)
-{
-    std::string err;
-
-    EC_POINT* rval;
-
-    BIGNUM* bn = BN_bin2bn(&bytes[0], bytes.size(), NULL);
-    if (!bn) {
-        err = "BN_bin2bn failed.";
-        goto finish;
-    }
-
-    rval = EC_POINT_bn2point(group, bn, point, ctx);
-    if (!rval) {
-        err = "EC_POINT_bn2point failed.";
-        goto finish;
-    }
-
-    finish:
-    if (bn) BN_clear_free(bn);
-
-    if (!err.empty()) {
-        throw std::runtime_error(std::string("secp256k1_point::set() - ") + err);
-    }
-}
-
-bytes_t secp256k1_point::bytes() const
-{
-    bytes_t bytes(33);
-
-    std::string err;
-
-    BIGNUM* rval;
+vector<unsigned char> Secp256k1_point::toBin() const {
+    vector<unsigned char> bytes(33);
 
     BIGNUM* bn = BN_new();
     if (!bn) {
-        err = "BN_new failed.";
-        goto finish;
+        throw runtime_error("secp256k1_point::toBin: bn_new failed!!");
     }
 
-    rval = EC_POINT_point2bn(group, point, POINT_CONVERSION_COMPRESSED, bn, ctx);
-    if (!rval) {
-        err = "EC_POINT_point2bn failed.";
-        goto finish;
+    if (!EC_POINT_point2bn(group, point, POINT_CONVERSION_COMPRESSED, bn, ctx)) {
+        BN_clear_free(bn);
+        throw runtime_error("secp256k1_point::toBin: point2bin failed!!");
     }
 
     assert(BN_num_bytes(bn) == 33);
     BN_bn2bin(bn, &bytes[0]);
-
-    finish:
-    if (bn) BN_clear_free(bn);
-
-    if (!err.empty()) {
-        throw std::runtime_error(std::string("secp256k1_point::get() - ") + err);
-    }
-
     return bytes;
 }
 
-secp256k1_point& secp256k1_point::operator+=(const secp256k1_point& rhs)
+Secp256k1_point& Secp256k1_point::operator+=(const Secp256k1_point& rhs)
 {
     if (!EC_POINT_add(group, point, point, rhs.point, ctx)) {
         throw std::runtime_error("secp256k1_point::operator+= - EC_POINT_add failed.");
@@ -133,7 +102,7 @@ secp256k1_point& secp256k1_point::operator+=(const secp256k1_point& rhs)
     return *this;
 }
 
-secp256k1_point& secp256k1_point::operator*=(const bytes_t& rhs)
+Secp256k1_point& Secp256k1_point::operator*=(const vector<unsigned char>& rhs)
 {
     BIGNUM* bn = BN_bin2bn(&rhs[0], rhs.size(), NULL);
     if (!bn) {
@@ -151,20 +120,20 @@ secp256k1_point& secp256k1_point::operator*=(const bytes_t& rhs)
 }
 
 // Computes n*G + K where K is this and G is the group generator
-void secp256k1_point::generator_mul(const bytes_t& n)
-{
-    BIGNUM* bn = BN_bin2bn(&n[0], n.size(), NULL);
-    if (!bn) throw std::runtime_error("secp256k1_point::generator_mul  - BN_bin2bn failed.");
-
-    //int rval = EC_POINT_mul(group, point, bn, (is_at_infinity() ? NULL : point), BN_value_one(), ctx);
-    int rval = EC_POINT_mul(group, point, bn, point, BN_value_one(), ctx);
-    BN_clear_free(bn);
-
-    if (rval == 0) throw std::runtime_error("secp256k1_point::generator_mul - EC_POINT_mul failed.");
-}
+//void secp256k1_point::generator_mul(const bytes_t& n)
+//{
+//    BIGNUM* bn = BN_bin2bn(&n[0], n.size(), NULL);
+//    if (!bn) throw std::runtime_error("secp256k1_point::generator_mul  - BN_bin2bn failed.");
+//
+//    //int rval = EC_POINT_mul(group, point, bn, (is_at_infinity() ? NULL : point), BN_value_one(), ctx);
+//    int rval = EC_POINT_mul(group, point, bn, point, BN_value_one(), ctx);
+//    BN_clear_free(bn);
+//
+//    if (rval == 0) throw std::runtime_error("secp256k1_point::generator_mul - EC_POINT_mul failed.");
+//}
 
 // Sets to n*G
-void secp256k1_point::set_generator_mul(const bytes_t& n)
+void Secp256k1_point::set_ng(const vector<unsigned char>& n)
 {
     BIGNUM* bn = BN_bin2bn(&n[0], n.size(), NULL);
     if (!bn) throw std::runtime_error("secp256k1_point::set_generator_mul  - BN_bin2bn failed.");
@@ -175,3 +144,8 @@ void secp256k1_point::set_generator_mul(const bytes_t& n)
     if (rval == 0) throw std::runtime_error("secp256k1_point::set_generator_mul - EC_POINT_mul failed.");
 }
 
+void Secp256k1_point::set_O() {
+    if (!EC_POINT_set_to_infinity(group, point)) {
+        throw runtime_error("secp256k1_point::set_0: EC_POINT_set_to_infinity failed!!");
+    }
+}
